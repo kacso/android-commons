b8c744d1a5ff4b1541a4fef5177c5d3955a95092
dsokac <dsokac1@gmail.com>
2019-06-24 15:46:50 +0200

    networkexceptionfactory unit tests

--- network/src/main/java/hr/dsokac/androidcommons/network/interceptors/NetworkExceptionInterceptor.kt
+++ network/src/main/java/hr/dsokac/androidcommons/network/interceptors/NetworkExceptionInterceptor.kt
@@ -1,10 +1,19 @@
 package hr.dsokac.androidcommons.network.interceptors
 
+import hr.dsokac.androidcommons.network.exceptions.NetworkException
 import hr.dsokac.androidcommons.network.exceptions.UnexpectedError
 import hr.dsokac.androidcommons.network.factories.INetworkExceptionFactory
 import okhttp3.Interceptor
 import okhttp3.Response
 
+/**
+ * OkHttp interceptor which which throws appropriate exceptions base on HTTP status code if there was ony error
+ * during network call. Otherwise, sergver response is returned.
+ *
+ * @property networkExceptionFactory Factory used to generate appropriate [NetworkException] in case of an error
+ *
+ * @author Danijel Sokač
+ */
 class NetworkExceptionInterceptor(
     private val networkExceptionFactory: INetworkExceptionFactory
 ) : Interceptor {
