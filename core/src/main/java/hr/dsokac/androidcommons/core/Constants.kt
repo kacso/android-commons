b22e8f6f4d9422c3ee1a28de50019430bdc0f62c
dsokac <danijel.sokac@inceptum.hr>
2019-07-18 13:51:39 +0200

    ListViewModel default implementation + TODOs

--- core/src/main/java/hr/dsokac/androidcommons/core/Constants.kt
+++ core/src/main/java/hr/dsokac/androidcommons/core/Constants.kt
@@ -1,3 +1,10 @@
 package hr.dsokac.androidcommons.core
 
 const val SNACKBAR_ELEVATION = 512f
+
+//TODO SearchableListFragment
+//TODO Adapters: (Multi)SelectListAdapter, SectionableListAdapter, (Multi)SelectSectionableListAdapter
+//TODO Views: BadgeView, SearchView
+//TODO Notification helpers
+//TODO Location module
+//TODO Tests
--- core/src/main/java/hr/dsokac/androidcommons/core/mvvm/viewmodels/ListViewModel.kt
+++ core/src/main/java/hr/dsokac/androidcommons/core/mvvm/viewmodels/ListViewModel.kt
@@ -1,8 +1,20 @@
 package hr.dsokac.androidcommons.core.mvvm.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 abstract class ListViewModel<T> : BaseViewModel() {
-    abstract fun getData(): LiveData<List<T>>
+    protected val liveData = MutableLiveData<List<T>>()
+
+    /**
+     * Default implementation will refresh data using [refreshData] function and return [liveData]
+     *
+     * @return [liveData]
+     */
+    open fun getData(): LiveData<List<T>> {
+        refreshData()
+        return liveData
+    }
+
     abstract fun refreshData()
 }
